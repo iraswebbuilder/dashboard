635790d8a222067e02964c34da7203d392b4a59c
iraswebbuilder <144859753+iraswebbuilder@users.noreply.github.com>
2023-11-23 19:05:01 +0700

    streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,299 +1,107 @@
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from tqdm.auto import tqdm
+# Set the aesthetic style of the plots
+sns.set_style('whitegrid')
 
+# We will create a pairplot to visualize the relationships between a subset of variables
+# Selecting a subset of variables for the pairplot
+variables = ['season', 'yr', 'mnth', 'holiday', 'weekday', 'workingday', 'weathersit', 'temp', 'atemp', 'hum', 'windspeed', 'cnt']
 
-# Menyiapkan data day_df
-day_df = pd.read_csv('day.csv')
-day_df.head()
+# Using tqdm to show progress
+for column in tqdm(variables):
+    df[column] = pd.to_numeric(df[column], errors='coerce')
 
-# Mengatur tema latar belakang gelap dan palet warna berwarna-warni
-sns.set(style='white', palette='deep')
+# Dropping any rows with NaN values that may have resulted from coercion
+pairplot_df = df.dropna(subset=variables)
+# Creating the pairplot
+sns.pairplot(pairplot_df[variables], hue='season', palette='viridis')
+plt.show()
+# Load the data
+df = pd.read_csv('day.csv')
+df['dteday'] = pd.to_datetime(df['dteday'])
 
-# Menghapus kolom yang tidak diperlukan
-drop_col = ['windspeed', 'instant']
+# Sort the dataframe by date
+sorted_df = df.sort_values('dteday')
 
-for i in day_df.columns:
-  if i in drop_col:
-    day_df.drop(labels=i, axis=1, inplace=True)
+# Streamlit title
+st.title('Total Daily Bike Rentals Over Time')
 
-# Mengubah nama judul kolom
-day_df.rename(columns={
-    'dteday': 'dateday',
-    'yr': 'year',
-    'mnth': 'month',
-    'weathersit': 'weather_cond',
-    'cnt': 'count'
-}, inplace=True)
+# Streamlit line chart
+st.line_chart(sorted_df.set_index('dteday')['cnt'])
+# Since the previous attempt to create a pairplot did not produce an output,
+# we will try again, ensuring that the data is processed correctly and efficiently.
 
-# Mengubah angka menjadi keterangan
-day_df['month'] = day_df['month'].map({
-    1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun',
-    7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'
-})
-day_df['season'] = day_df['season'].map({
-    1: 'Spring', 2: 'Summer', 3: 'Fall', 4: 'Winter'
-})
-day_df['weekday'] = day_df['weekday'].map({
-    0: 'Sun', 1: 'Mon', 2: 'Tue', 3: 'Wed', 4: 'Thu', 5: 'Fri', 6: 'Sat'
-})
-day_df['weather_cond'] = day_df['weather_cond'].map({
-    1: 'Clear/Partly Cloudy',
-    2: 'Misty/Cloudy',
-    3: 'Light Snow/Rain',
-    4: 'Severe Weather'
-})
+# We will use a smaller subset of variables for the pairplot to ensure it can be processed
+variables = ['season', 'temp', 'atemp', 'hum', 'windspeed', 'cnt']
 
-# Menyiapkan daily_rent_df
-def create_daily_rent_df(df):
-    daily_rent_df = df.groupby(by='dateday').agg({
-        'count': 'sum'
-    }).reset_index()
-    return daily_rent_df
+# Convert 'season' to a categorical type for better plotting
+pairplot_df['season'] = pairplot_df['season'].astype('category')
 
-# Menyiapkan daily_casual_rent_df
-def create_daily_casual_rent_df(df):
-    daily_casual_rent_df = df.groupby(by='dateday').agg({
-        'casual': 'sum'
-    }).reset_index()
-    return daily_casual_rent_df
+# Creating the pairplot with a smaller subset of variables
+sns.pairplot(pairplot_df[variables], hue='season', palette='viridis', markers='+')
+plt.show()
+# Correcting the code to ensure the pairplot is displayed
+# First, we ensure that 'season' is converted to a categorical type correctly
 
-# Menyiapkan daily_registered_rent_df
-def create_daily_registered_rent_df(df):
-    daily_registered_rent_df = df.groupby(by='dateday').agg({
-        'registered': 'sum'
-    }).reset_index()
-    return daily_registered_rent_df
+# Convert 'season' to a categorical type for better plotting
+pairplot_df = df.dropna(subset=variables)
+pairplot_df['season'] = pairplot_df['season'].astype('category')
 
-# Menyiapkan season_rent_df
-def create_season_rent_df(df):
-    season_rent_df = df.groupby(by='season')[['registered', 'casual']].sum().reset_index()
-    return season_rent_df
+# Now, we create the pairplot with the corrected dataframe
+sns.pairplot(pairplot_df[variables], hue='season', palette='viridis', markers='+')
+plt.show()
+# Let's explore the usage patterns of the bike sharing system across different seasons.
+# We will create a boxplot to compare the distribution of total bike rentals ('cnt') across different seasons.
 
-# Menyiapkan monthly_rent_df
-def create_monthly_rent_df(df):
-    monthly_rent_df = df.groupby(by='month').agg({
-        'count': 'sum'
-    })
-    ordered_months = [
-        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-    ]
-    monthly_rent_df = monthly_rent_df.reindex(ordered_months, fill_value=0)
-    return monthly_rent_df
+# First, we convert 'season' to a more readable format
+seasons = {1: 'Winter', 2: 'Spring', 3: 'Summer', 4: 'Fall'}
+pairplot_df['season_name'] = pairplot_df['season'].map(seasons)
 
-# Menyiapkan weekday_rent_df
-def create_weekday_rent_df(df):
-    weekday_rent_df = df.groupby(by='weekday').agg({
-        'count': 'sum'
-    }).reset_index()
-    return weekday_rent_df
+# Now, we create the boxplot
+plt.figure(figsize=(10, 6))
+sns.boxplot(x='season_name', y='cnt', data=pairplot_df, palette='coolwarm')
+plt.title('Bike Rentals by Season')
+plt.xlabel('Season')
+plt.ylabel('Total Bike Rentals')
+plt.show()
+# To further understand the data, we will perform a time series decomposition
+# to observe the trend, seasonality, and residuals of the bike rentals over time.
+from statsmodels.tsa.seasonal import seasonal_decompose
 
-# Menyiapkan workingday_rent_df
-def create_workingday_rent_df(df):
-    workingday_rent_df = df.groupby(by='workingday').agg({
-        'count': 'sum'
-    }).reset_index()
-    return workingday_rent_df
+# We need to ensure our data is in a time series format, with the date as the index
+# and the total bike rentals ('cnt') as the values.
+time_series_df = pairplot_df.set_index('dteday')['cnt']
 
-# Menyiapkan holiday_rent_df
-def create_holiday_rent_df(df):
-    holiday_rent_df = df.groupby(by='holiday').agg({
-        'count': 'sum'
-    }).reset_index()
-    return holiday_rent_df
+# The frequency is daily, so we will set the model's frequency to 1
+decomposition = seasonal_decompose(time_series_df, model='additive', period=1)
 
-# Menyiapkan weather_rent_df
-def create_weather_rent_df(df):
-    weather_rent_df = df.groupby(by='weather_cond').agg({
-        'count': 'sum'
-    })
-    return weather_rent_df
+# Plotting the decomposed time series components
+trend = decomposition.trend
+trend.name = 'Trend'
+seasonal = decomposition.seasonal
+seasonal.name = 'Seasonality'
+residual = decomposition.resid
+residual.name = 'Residuals'
 
-# Membuat komponen filter
-min_date = pd.to_datetime(day_df['dateday']).dt.date.min()
-max_date = pd.to_datetime(day_df['dateday']).dt.date.max()
-
-# Mengambil start_date & end_date dari date_input
-start_date, end_date = st.date_input(
-    label='Rentang Waktu',
-    min_value=min_date,
-    max_value=max_date,
-    value=[min_date, max_date]
-)
-
-# Filter DataFrame berdasarkan start_date dan end_date
-day_df = day_df[(day_df['dateday'] >= str(start_date)) &
-                (day_df['dateday'] <= str(end_date))]
-
-# Menyiapkan berbagai dataframe
-daily_rent_df = create_daily_rent_df(day_df)
-daily_casual_rent_df = create_daily_casual_rent_df(day_df)
-daily_registered_rent_df = create_daily_registered_rent_df(day_df)
-season_rent_df = create_season_rent_df(day_df)
-monthly_rent_df = create_monthly_rent_df(day_df)
-weekday_rent_df = create_weekday_rent_df(day_df)
-workingday_rent_df = create_workingday_rent_df(day_df)
-holiday_rent_df = create_holiday_rent_df(day_df)
-weather_rent_df = create_weather_rent_df(day_df)
-
-# Membuat Dashboard secara lengkap
-
-# Membuat judul
-st.header('DASHBOARD PENYEWAAN SEPEDA')
-
-# Membuat jumlah penyewaan harian
-st.subheader('Daily Rentals')
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    daily_rent_casual = daily_casual_rent_df['casual'].sum()
-    st.metric('Casual User', value=daily_rent_casual)
-
-with col2:
-    daily_rent_registered = daily_registered_rent_df['registered'].sum()
-    st.metric('Registered User', value=daily_rent_registered)
-
-with col3:
-    daily_rent_total = daily_rent_df['count'].sum()
-    st.metric('Total User', value=daily_rent_total)
-
-# Membuat jumlah penyewaan bulanan
-st.subheader('Monthly Rentals')
-fig, ax = plt.subplots(figsize=(24, 8))
-ax.plot(
-    monthly_rent_df.index,
-    monthly_rent_df['count'],
-    marker='o',
-    linewidth=2,
-    color='tab:red'
-)
-
-for index, row in enumerate(monthly_rent_df['count']):
-    ax.text(index, row + 1, str(row), ha='center', va='bottom', fontsize=12)
-
-ax.tick_params(axis='x', labelsize=25, rotation=45)
-ax.tick_params(axis='y', labelsize=20)
-st.pyplot(fig)
-
-# Membuat jumlah penyewaan berdasarkan season
-st.subheader('Seasonly Rentals')
-
-fig, ax = plt.subplots(figsize=(16, 8))
-
-sns.barplot(
-    x='season',
-    y='registered',
-    data=season_rent_df,
-    label='Registered',
-    color='tab:red',
-    ax=ax
-)
-
-sns.barplot(
-    x='season',
-    y='casual',
-    data=season_rent_df,
-    label='Casual',
-    color='tab:grey',
-    ax=ax
-)
-
-for index, row in season_rent_df.iterrows():
-    ax.text(index, row['registered'], str(row['registered']), ha='center', va='bottom', fontsize=12)
-    ax.text(index, row['casual'], str(row['casual']), ha='center', va='bottom', fontsize=12)
-
-ax.set_xlabel(None)
-ax.set_ylabel(None)
-ax.tick_params(axis='x', labelsize=20, rotation=0)
-ax.tick_params(axis='y', labelsize=15)
-ax.legend()
-st.pyplot(fig)
-
-# Membuah jumlah penyewaan berdasarkan kondisi cuaca
-st.subheader('Weatherly Rentals')
-
-fig, ax = plt.subplots(figsize=(16, 8))
-
-# Menggunakan warna hex
-colors=["#FF0000", "#00FF00", "#0000FF"]
-
-sns.barplot(
-    x=weather_rent_df.index,
-    y=weather_rent_df['count'],
-    palette=colors,
-    ax=ax
-)
-
-for index, row in enumerate(weather_rent_df['count']):
-    ax.text(index, row + 1, str(row), ha='center', va='bottom', fontsize=12)
-
-ax.set_xlabel(None)
-ax.set_ylabel(None)
-ax.tick_params(axis='x', labelsize=20)
-ax.tick_params(axis='y', labelsize=15)
-st.pyplot(fig)
-
-# Membuat jumlah penyewaan berdasarkan weekday, working dan holiday
-st.subheader('Weekday, Workingday, and Holiday Rentals')
-
-fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(15,10))
-
-colors1=["tab:red", "tab:blue"]
-colors2=["tab:red", "tab:blue"]
-colors3=["tab:red", "tab:blue", "tab:green", "tab:orange", "tab:purple", "tab:brown", "tab:pink"]
-
-# Berdasarkan workingday
-sns.barplot(
-    x='workingday',
-    y='count',
-    data=workingday_rent_df,
-    palette=colors3,
-    ax=axes[0])
-
-for index, row in enumerate(workingday_rent_df['count']):
-    axes[0].text(index, row + 1, str(row), ha='center', va='bottom', fontsize=12)
-
-axes[0].set_title('Number of Rents based on Working Day')
-axes[0].set_ylabel(None)
-axes[0].tick_params(axis='x', labelsize=15)
-axes[0].tick_params(axis='y', labelsize=10)
-
-# Berdasarkan holiday
-sns.barplot(
-  x='holiday',
-  y='count',
-  data=holiday_rent_df,
-  palette=colors1,
-  ax=axes[1])
-
-for index, row in enumerate(holiday_rent_df['count']):
-    axes[1].text(index, row + 1, str(row), ha='center', va='bottom', fontsize=12)
-
-axes[1].set_title('Number of Rents based on Holiday')
-axes[1].set_ylabel(None)
-axes[1].tick_params(axis='x', labelsize=15)
-axes[1].tick_params(axis='y', labelsize=10)
-
-# Berdasarkan weekday
-sns.barplot(
-  x='weekday',
-  y='count',
-  data=weekday_rent_df,
-  palette=colors2,
-  ax=axes[2])
-
-for index, row in enumerate(weekday_rent_df['count']):
-    axes[2].text(index, row + 1, str(row), ha='center', va='bottom', fontsize=12)
-
-axes[2].set_title('Number of Rents based on Weekday')
-axes[2].set_ylabel(None)
-axes[2].tick_params(axis='x', labelsize=15)
-axes[2].tick_params(axis='y', labelsize=10)
-
+# Plotting all the components
+plt.figure(figsize=(14, 8))
+plt.subplot(411)
+plt.plot(time_series_df, label='Original')
+plt.legend(loc='upper left')
+plt.subplot(412)
+plt.plot(trend, label='Trend')
+plt.legend(loc='upper left')
+plt.subplot(413)
+plt.plot(seasonal,label='Seasonality')
+plt.legend(loc='upper left')
+plt.subplot(414)
+plt.plot(residual, label='Residuals')
+plt.legend(loc='upper left')
 plt.tight_layout()
-st.pyplot(fig)
+plt.show()
 
-st.caption('Copyright (c) ARIS SUTIONO 2023')
+# Copyright notice
+st.markdown('Copyright ARIS SUTIONO 2023')
